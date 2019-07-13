4020054f39dee23733243d597ea496746fb146ad
unknown <rizkyjumadi29@gmail.com>
2019-07-14 05:09:59 +0800

    menambahkan file js baru

---
+++ script.js
@@ -0,0 +1,114 @@
+// variabel
+// var nama = "rofiudin"
+
+// deklarasi array indexed
+// var mahasiswa = ["ilmi", "hanif", "maulana"]
+
+// perulangan
+// for (var i = 0; i < mahasiswa.length; i++) {
+// 	console.log("Nama ke-"+i+" adalah "+mahasiswa[i])
+// }
+
+
+// logika
+
+// var nilai=90
+// var standar=85
+
+// if (nilai > standar) {
+// 	console.log("selamat anda lulus")
+// }else{
+// 	console.log("maaf anda mengulang semester depan")
+// }
+
+
+// var username = "admin"
+// var password = "passsalah"
+
+// if (username == "admin" && password == "tampan") {
+// 	console.log("berhasil login")
+// }else{
+// 	console.log("gagal login")
+// }
+
+
+// FUNGSI
+
+// function siapaAku(nama = "bejo", alamat = "penajam") {
+// 	console.log("Nama aku adalah "+nama+" aku tinggal di "+alamat)
+// }
+
+// siapaAku("hanif", "gunsel")
+
+
+
+// FUNGSI yang me-return sebuah value
+// function ambilUmur() {
+// 	return 21
+// }
+
+// var umur = ambilUmur()
+// console.log(umur)
+
+
+// function ambilAlamat() {
+// 	return "Jalan sokle"
+// }
+
+// console.log(ambilAlamat())
+
+
+// function cekRumahNya(nama) {
+// 	nama = nama.toLowerCase()
+// 	if (nama == "rofi") {
+// 		console.log("oh ini mah orang gunsel")
+// 	}else if (nama == "ilmi") {
+// 		console.log("oh ini je orang kerok laut")
+// 	}else if (nama == "hanif") {
+// 		console.log("dekat masjid rumahnya ini")
+// 	}else if (nama == "ibnu") {
+// 		console.log("samping papadaan rumahnya ini enak alah")
+// 	}else if (nama == "adit") {
+// 		console.log("di kilo 2 dekat global")
+// 	}else{
+// 		console.log("nah ndaktau sudah ini dimana rumahnya")
+// 	}
+// }
+
+// cekRumahNya("rofi")
+
+
+
+// Array associative
+
+// var alamat = {
+// 	"rofi": "losari",
+// 	"adit": "kilo 2",
+// 	"maulana": "lawe-lawe"
+// }
+
+
+// alamat.rofi
+
+
+// var pejabat = [
+// {
+// 	"nama": "Abdul Gafur Masud", 
+// 	"alamat": "Balikpapan"
+// },
+// {
+// 	"nama": "Hamdan", 
+// 	"alamat": "AMD 1"
+// },
+// {
+// 	"nama": "Andi Harahap", 
+// 	"alamat": "Penajam"
+// },
+// {
+// 	"nama": "Datu Adam", 
+// 	"alamat": "Nipah-nipah"
+// }
+// ]
+
+
+
